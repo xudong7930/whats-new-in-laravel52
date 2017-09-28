84b64341445a5505497c46b68d697a430925bfb7
xudong7930 <xudong7930@gmail.com>
2017-09-28 08:07:09 +0800

    api rate limit

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,4 +1,12 @@
 <?php
+// api rate limit
+Route::get('/api/search/{item}', function($item) {
+    return [
+        'result' => $item
+    ];
+})->middleware('throttle:3');
+
+// implict route binding
 Route::get('user/{user}', function(App\User $user) {
     return $user;
 });
